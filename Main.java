946eb2fe935d1ea5dffbaa7c1bfc6f349adecbf6
ArtekII <216646542+ArtekII@users.noreply.github.com>
2026-05-21 12:46:29 +0300

    Init

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // donnees d'entree
+        // (fonction objectif, contraintes, etc.)
+
+        // algorithme simplex a deux phase
+    }
+}
+
+// methode qui transforme un probleme de programmation lineaire en un probleme de programmation lineaire standard
+// en ajoutant des variables d'ecart et des variables artificielles si necessaire
+// et en reformulant les contraintes pour les rendre compatibles avec l'algorithme simplex
+
+// methode qui verifie l'egalite/inegalite des contraintes et qui determine les variables d'ecart et les variables artificielles a ajouter
+
+// methode qui dresse le tableau du simplex a partir du probleme de programmation lineaire standard
+
+// methode qui determine la variable entrante et sortante de la base a chaque iteration de l'algorithme simplex
+
+// methode qui effectue les operations de pivot pour trouver la solution optimale du probleme de programmation lineaire standard
+
+// methode qui verifie les conditions d'arret de l'algorithme simplex (solution optimale trouvee, probleme non borne, etc.)
